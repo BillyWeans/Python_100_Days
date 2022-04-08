1a4faaf8486a6d8286e93d62dcefe4e4390423d8
BillyWeans <WillyBeans81@hotmail.com>
2022-04-07 20:27:27 -0400

    PasswordManager: Adding exception handling / JSON saving

--- Day26/NATO-alphabet/main.py
+++ Day26/NATO-alphabet/main.py
@@ -7,8 +7,17 @@ df_nato_alpha = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 dict_nato_alpha = {row.letter:row.code for (index, row) in df_nato_alpha.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_name = input("What is your name? ").upper()
-print(list(user_name))
-user_nato = [dict_nato_alpha[letter] for letter in user_name]
-print(user_nato)
+
+def generate_nato_name():
+    # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+    user_name = input("What is your name? ").upper()
+    try:
+        user_nato = [dict_nato_alpha[letter] for letter in user_name]
+    except KeyError:
+        print("Please use only alphabetic characters")
+        generate_nato_name()
+    else:
+        print(user_nato)
+
+
+generate_nato_name()
--- Day29/PasswordManager/main.py
+++ Day29/PasswordManager/main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 import random
@@ -6,7 +7,7 @@ import pyperclip
 WHITE = "white"
 BLACK = "black"
 GRAY = "gray"
-FONT_LABEL=("Courier", 15, "bold")
+FONT_LABEL = ("Courier", 15, "bold")
 
 
 def generate_password():
@@ -43,19 +44,56 @@ def add_entry():
         return
 
     save_confirm = messagebox.askyesno(title="Save Password?", message=f"Website: {website}\n"
-                                       f"Username: {email_user}\n "
-                                       f"Password: {passwd}\n\n"
-                                       f"Are you sure you want to save this data?")
+                                                                       f"Username: {email_user}\n "
+                                                                       f"Password: {passwd}\n\n"
+                                                                       f"Are you sure you want to save this data?")
     if save_confirm:
-        with open("data.txt", mode="a") as f_pass:
-            entry = f"{website} | {email_user} | {passwd}\n"
-            f_pass.write(entry)
+        new_passwd_data = {website:
+                    {"username": email_user,
+                     "password": passwd
+                     }
+                }
 
-        entry_website.delete(0, END)
-        entry_passwd.delete(0, END)
-        entry_website.focus()
+        # Open Password File
+        try:
+            with open("data.json", mode="r") as f_pass:
+                old_passwd_data = json.load(f_pass)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as f_pass:
+                json.dump(new_passwd_data, f_pass, indent=4)
+        else:
+            # Update our json object with the existing file data
+            new_passwd_data.update(old_passwd_data)
+            # Save/Update Password File
+            with open("data.json", mode="w") as f_pass:
+                json.dump(new_passwd_data, f_pass, indent=4)
+        finally:
+            entry_website.delete(0, END)
+            entry_passwd.delete(0, END)
+            entry_website.focus()
 
-        pyperclip.copy(passwd)
+            pyperclip.copy(passwd)
+
+
+def search_passwds():
+    search_website = entry_website.get()
+
+    try:
+        with open("data.json") as p_file:
+            password_entries = json.load(p_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="File Not Found", message="File 'data.json' could not be found!")
+        return
+
+    try:
+        password_entry = password_entries[search_website]
+    except KeyError:
+        messagebox.showerror(title="No matching website", message=f"Website '{search_website}' was not found")
+        return
+
+    messagebox.showinfo(title="Match Found", message=f"Website: {search_website}\n"
+                        f"Username: {password_entry['username']}\n"
+                        f"Password: {password_entry['password']}\n")
 
 
 window = Tk()
@@ -71,16 +109,21 @@ lbl_website = Label(text="Website", bg=WHITE, fg=BLACK, font=FONT_LABEL)
 lbl_email_user = Label(text="Email/Username", bg=WHITE, fg=BLACK, font=FONT_LABEL)
 lbl_passwd = Label(text="Password", bg=WHITE, fg=BLACK, font=FONT_LABEL)
 
-entry_website = Entry(width=43, bg=WHITE, fg=BLACK, highlightbackground=GRAY, highlightthickness=1)
+entry_website = Entry(width=22, bg=WHITE, fg=BLACK, highlightbackground=GRAY, highlightthickness=1)
 entry_email_user = Entry(width=43, bg=WHITE, fg=BLACK, highlightbackground=GRAY, highlightthickness=1)
 entry_passwd = Entry(width=22, bg=WHITE, fg=BLACK, highlightbackground=GRAY, highlightthickness=1)
 
-btn_gen_passwd = Button(text="Generate Password", bg=WHITE, fg=BLACK, highlightbackground=WHITE, font=FONT_LABEL, command=generate_password)
-btn_add = Button(width=35, text="Add", bg=WHITE, fg=BLACK, highlightbackground=WHITE, font=FONT_LABEL, command=add_entry)
+btn_gen_passwd = Button(text="Generate Password", bg=WHITE, fg=BLACK, highlightbackground=WHITE, font=FONT_LABEL,
+                        command=generate_password)
+btn_add = Button(width=35, text="Add", bg=WHITE, fg=BLACK, highlightbackground=WHITE, font=FONT_LABEL,
+                 command=add_entry)
+btn_search = Button(width=17, text="Search", bg=WHITE, fg=BLACK, highlightbackground=WHITE, font=FONT_LABEL,
+                    command=search_passwds)
 
 canvas.grid(column=1, row=0, columnspan=2)
 lbl_website.grid(column=0, row=1)
-entry_website.grid(column=1, row=1, columnspan=2)
+entry_website.grid(column=1, row=1, columnspan=1)
+btn_search.grid(column=2, row=1)
 lbl_email_user.grid(column=0, row=2)
 entry_email_user.grid(column=1, row=2, columnspan=2)
 lbl_passwd.grid(column=0, row=3)
@@ -88,7 +131,7 @@ entry_passwd.grid(column=1, row=3)
 btn_gen_passwd.grid(column=2, row=3)
 btn_add.grid(column=1, row=4, columnspan=2)
 
-entry_email_user.insert(0, "latterkw@gmail.com") # 0 at the beginning indicates at the start of the text box, as opposed to END at the...end
+entry_email_user.insert(0, "latterkw@gmail.com")  # 0 at the beginning indicates at the start of the text box, as opposed to END at the...end
 
 entry_website.focus()
 
